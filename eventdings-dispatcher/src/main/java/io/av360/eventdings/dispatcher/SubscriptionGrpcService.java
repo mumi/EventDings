5b7eede07c3b31e4305dec3dc57bcdfeb8690a65
mumi <m.polatkan@outlook.de>
2023-02-06 14:52:45 +0100

    Changed architecture

--- eventdings-dispatcher/src/main/java/io/av360/eventdings/dispatcher/SubscriptionGrpcService.java
+++ eventdings-dispatcher/src/main/java/io/av360/eventdings/dispatcher/SubscriptionGrpcService.java
@@ -1,11 +1,20 @@
 package io.av360.eventdings.dispatcher;
 
-import io.av360.eventdings.grpc;
+import io.av360.eventdings.grpc.SubscriptionServiceGrpc;
+import io.av360.eventdings.grpc.HelloRequest;
+import io.av360.eventdings.grpc.HelloReply;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.stereotype.Service;
 
 @GrpcService
+ @Service
 public class SubscriptionGrpcService extends SubscriptionServiceGrpc.SubscriptionServiceImplBase {
+
+    public SubscriptionGrpcService() {
+
+        System.out.println(">> SubscriptionGrpcService");
+    }
 
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
