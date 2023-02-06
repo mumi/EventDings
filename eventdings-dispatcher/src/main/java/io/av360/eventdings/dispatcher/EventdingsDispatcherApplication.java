7bbee1bfe0b14a87434e81cefc05a81f1c36108a
mumi <m.polatkan@outlook.de>
2023-02-06 16:29:11 +0100

    Fixed versions

--- eventdings-dispatcher/src/main/java/io/av360/eventdings/dispatcher/EventdingsDispatcherApplication.java
+++ eventdings-dispatcher/src/main/java/io/av360/eventdings/dispatcher/EventdingsDispatcherApplication.java
@@ -1,13 +1,8 @@
 package io.av360.eventdings.dispatcher;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class EventdingsDispatcherApplication {
--- eventdings-dispatcher/src/main/java/io/av360/eventdings/dispatcher/GrpcServerService.java
+++ eventdings-dispatcher/src/main/java/io/av360/eventdings/dispatcher/GrpcServerService.java
@@ -1,21 +1,13 @@
 package io.av360.eventdings.dispatcher;
 
-import io.av360.eventdings.grpc.SubscriptionServiceGrpc;
+import io.av360.eventdings.grpc.SubscriptionGrpc;
 import io.av360.eventdings.grpc.HelloRequest;
 import io.av360.eventdings.grpc.HelloReply;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
-import org.springframework.stereotype.Service;
 
 @GrpcService
- @Service
-public class SubscriptionGrpcService extends SubscriptionServiceGrpc.SubscriptionServiceImplBase {
-
-    public SubscriptionGrpcService() {
-
-        System.out.println(">> SubscriptionGrpcService");
-    }
-
+public class GrpcServerService extends SubscriptionGrpc.SubscriptionImplBase {
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
         HelloReply reply = HelloReply.newBuilder()
